25f1c3681150f617ec8dd0d059afc581d145e868
juanmanuelrojaspai-beep <juanmanuelrojaspai@gmail.com>
2026-09-18 21:08:27 -0500

    Update project title and logical operator labels

--- Inicio.py
+++ Inicio.py
@@ -2,17 +2,17 @@ import streamlit as st
 
 def inicio ():
     st.title("Inicio")
-    st.markdown("## Proyecto de prueba de carga")
+    st.markdown("## Proyecto Matematicas")
 
     st.divider()
 
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        st.markdown("### prub 1 ")
+        st.markdown("### operador logico and  ")
     with col2:
-        st.markdown("### columna 2")
+        st.markdown("### operador logico or")
     with col3:
-        st.markdown("### prub 3")
+        st.markdown("### operador logico not")
 
     st.divider()
